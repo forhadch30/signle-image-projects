aebf1c7375520d7c37c1c1283f5438ff4c44963e
forhad30 <forhadchariani30@gmail.com>
2024-09-02 09:34:41 +0600

    simple for and for loop and while loop

---
+++ for.js
@@ -0,0 +1,20 @@
+for (let n = 0; n <= 10; n++) {
+    console.log(n);
+}
+// event number 
+for (i = 2; i <= 20; i = i + 2) {
+    console.log(i);
+}
+// odd number 
+for (i = 1; i <= 20; i = i + 2) {
+    console.log(i);
+}
+
+let num = 1;
+while (num <= 5) {
+    console.log("Assalamu alaikum");
+    num++
+}
+for (let i = 0; i <= 5; i++) {
+    console.log("Assalamu alaikum");
+}
