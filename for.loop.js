a19b740a327c422d842d126f490d48b539b64a8c
forhad30 <forhadchariani30@gmail.com>
2024-09-02 08:15:21 +0600

    for-loop and range and sum

---
+++ for.loop.js
@@ -0,0 +1,5 @@
+const numbers = [12, 32, 43, 54, 65, 76, 87, 89, 34,];
+console.log(numbers);
+for (const number of numbers) {
+    console.log(number);
+}
---
+++ while-loop.js
@@ -0,0 +1,11 @@
+const number = 10;
+if (number < 9) {
+    console.log('very good');
+}
+console.log('sed my mon');
+
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++;
+}
